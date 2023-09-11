6fe3fa18648371b9afb82f8e170f0cc85990da1c
Yeasin7772 <yeasin26yeasin26@gmail.com>
2023-09-12 00:06:21 +0600

    first commit

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,9 @@
+const Bookmarks = () => {
+    return (
+        <div className="md:w-1/3">
+            <h1 className="text-xl font-bold">Bookmarks : </h1>
+        </div>
+    );
+};
+
+export default Bookmarks;
